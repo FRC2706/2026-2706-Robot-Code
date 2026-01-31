1124df7ce955e80eed247b5334aedcce1aac28f4
Ethan <ethan.soh360@gmail.com>
2026-01-30 20:01:53 -0500

    Added photonlib vendor dependency, added necessary imports and setup for PhotonSubsystem and PhotonMoveToTarget

--- 2026-2706-Robot-Code/src/main/java/frc/robot/commands/PhotonMoveToTarget.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/commands/PhotonMoveToTarget.java
@@ -1,4 +1,14 @@
-public class PhotonMoveToTarget {
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+// Imports
+import edu.wpi.first.wpilibj2.command.Command;
+
+// Class
+public class PhotonMoveToTarget extends Command {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
@@ -20,6 +30,6 @@ public class PhotonMoveToTarget {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        
+        return false; // Placeholder, change later
     }
 }
--- 2026-2706-Robot-Code/src/main/java/frc/robot/subsystems/PhotonSubsystem.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/subsystems/PhotonSubsystem.java
@@ -1,4 +1,14 @@
-public class PhotonSubsystem {
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+// Imports
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// Class
+public class PhotonSubsystem extends SubsystemBase {
     
     @Override
     public void periodic() {
