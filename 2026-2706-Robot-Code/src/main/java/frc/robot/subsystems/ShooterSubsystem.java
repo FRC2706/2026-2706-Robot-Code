28008114e5acb016e2277f6c03831bbd9d4e0010
Aadhya Sure <aadhya.sure@gmail.com>
2026-02-11 20:21:52 -0500

    Deleted unecessary imports in shooter subsytstem.

--- 2026-2706-Robot-Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,17 +1,8 @@
 package frc.robot.subsystems;
-
-import com.revrobotics.CANSparkBase.ControlType;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import static frc.robot.subsystems.ShooterStateMachine.ShooterModes.*;
 import static frc.robot.subsystems.ShooterStateMachine.States.*;
 
 import java.util.function.BooleanSupplier;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkPIDController;
 
 import edu.wpi.first.networktables.BooleanPublisher;
 import edu.wpi.first.networktables.DoublePublisher;
@@ -23,9 +14,6 @@ import edu.wpi.first.networktables.StringPublisher;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.lib.lib2706.ErrorCheck;
-import frc.lib.lib2706.TunableNumber;
-import frc.robot.Config;
 import frc.robot.subsystems.ShooterStateMachine.ShooterModes;
 import frc.robot.subsystems.ShooterStateMachine.States;
 
@@ -36,9 +24,8 @@ public class ShooterSubsystem extends SubsystemBase {
   public ShooterSubsystem() {}
 
   /**
-   * Example command factory method.
-   *
-   * @return a command
+  
+   * @return 
    */
   public Command exampleMethodCommand() {
     return runOnce(
