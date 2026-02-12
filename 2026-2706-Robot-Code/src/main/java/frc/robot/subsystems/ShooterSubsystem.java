bb0708ac9d101efaebb3a3f197d9ec6ef4994946
AnishaJ44 <anisharj44@gmail.com>
2026-02-11 20:55:30 -0500

    added motor names

--- 2026-2706-Robot-Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,35 +1,18 @@
 package frc.robot.subsystems;
-import static frc.robot.subsystems.ShooterStateMachine.ShooterModes.*;
-import static frc.robot.subsystems.ShooterStateMachine.States.*;
-
-import java.util.function.BooleanSupplier;
-
-import edu.wpi.first.networktables.BooleanPublisher;
-import edu.wpi.first.networktables.DoublePublisher;
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.PubSubOption;
-import edu.wpi.first.networktables.StringPublisher;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.ShooterStateMachine.ShooterModes;
-import frc.robot.subsystems.ShooterStateMachine.States;
-
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkClosedLoopController;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 
 public class ShooterSubsystem extends SubsystemBase {
-  private SparkMax m_motor;
-  private SparkMax m_motor2;
+  private SparkMax shooterMotor1;
+  private SparkMax shooterMotor2;
+  private SparkMax feederMotor; // CANNOT be faster than shooterMotor RPM
+  private SparkMax indexerMotor;
   
   public ShooterSubsystem() {}
-
   /**
   
    * @return 
