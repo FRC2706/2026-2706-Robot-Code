32ced667248f7da4dc8e9cb9a7f80bbc0fdb00b8
apaul10-dot <orcaman06@gmail.com>
2026-02-28 14:36:28 -0500

    Remove SwerveSubsystem and PhotonSubsystem references
    Commented out SwerveSubsystem and PhotonSubsystem instantiation.

--- 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,6 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
-//import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.PhotonSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -23,8 +22,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  //private final SwerveSubsystem m_SwerveSubsystem = new SwerveSubsystem(null);
-  //private final PhotonSubsystem m_PhotonSubsystem = new PhotonSubsystem(m_SwerveSubsystem);
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
