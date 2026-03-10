2952a1b5c6fa8d5cd87c2c491b16487442fe7635
GavinWu42711 <gavinwu42711@gmail.com>
2026-03-09 21:57:04 -0400

    Swerve (#46)
    * Setup
    
    - Copied over the code from Swerve Beetle (without the pathplanner and photon vision code)
    - Added in the dimensions of the modules to the center of the robot and the diameter of the wheels
    
    * Module Locations
    
    The intake system was changed to be the front of the robot instead of the direction where the robot is shooting
    
    * Bugfix
    
    - Bug fix with updating encoder with absolute encoder
    - Added in deadband
    
    * Pathplanner & Drift
    
    - Added in method's necessary for pathplanner to function
    - Linked the "start" button on the controller to reseting the gyro
    - Turned heading correction and cosine-compensator on to help with drift issues
    
    * Drift
    
    set angularVelocityCorrection and autonomousAngularVelocityCorrection to true to help reduce drift that is present
    
    * Config Files
    
    Added in the gear ratios for angle and drive
    
    * Config File
    
    Edited some of the physical properties of the config file
    
    * YAGSL config
    
    updated the weight of the robot
    
    * Config File
    
    - Updated config file and dependencies
    
    * Bug-fixing
    
    Removed some of the pathplanner integration since it was having some bugs
    
    * Comments
    
    Added some more comments for clearer explanation
    
    * Clean-up
    
    Cleaning up some unecessary code
    
    * PID tuning
    
    PID tuning
    
    * PID
    
    PID
    
    * PID
    
    PID

--- 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,10 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 
 import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
-
+import frc.robot.commands.SwerveDriveCommand;
+import frc.robot.commands.ResetGyroCommand;
 
 import frc.robot.commands.IntakeDownCommand;
 import frc.robot.commands.IntakeUpCommand;
@@ -18,12 +20,21 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.PhotonSubsystem;
 import frc.robot.subsystems.AutoSelectorKnobSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 
 import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import java.io.File;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
@@ -35,39 +46,75 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
 
   // Subsystem
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
   private final AutoSelectorKnobSubsystem m_AutoSelectorKnobSubsystem = new AutoSelectorKnobSubsystem();
+  private final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
 
   // Controller
-  private final XboxController driverController = new XboxController(0);
+  private final CommandXboxController driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-      configureButtonBindings();
+
+    // Left joystick controls the robot's translation movements (up moves the robot up, left moves the robot left, e.t.c)
+    // Right joystick controls the rate of rotation (left rotates the robot counter clock-wise, right rotates the robot clock-wise)
+    m_swerveSubsystem.setDefaultCommand(
+      new SwerveDriveCommand(m_swerveSubsystem,
+       () -> -driverController.getLeftY(), 
+       () -> -driverController.getLeftX(), 
+       () -> -driverController.getRightX(),
+       0.1,
+       0.1
+       )
+    );
+
+    // Configure the trigger bindings
+    configureBindings();
   }
 
-  private void configureButtonBindings() {
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-        // Toggle intake ON/OFF
-        JoystickButton intakeToggleButton =
-            new JoystickButton(driverController, XboxController.Button.kA.value);
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
 
-        // Run intake in reverse while held
-        JoystickButton reverseButton =
-            new JoystickButton(driverController, XboxController.Button.kB.value);
+    //Zero the gyro such that forward is where the robot is currently looking
+    driverController.start().onTrue(new ResetGyroCommand(m_swerveSubsystem));
 
-        // Toggle intake to go down
-        JoystickButton intakeDownButton =
-            new JoystickButton(driverController, XboxController.Button.kX.value);
+    // Toggle intake ON/OFF
+    JoystickButton intakeToggleButton =
+        new JoystickButton(driverController.getHID(), XboxController.Button.kA.value);
 
-        // Toggle intake to go up
-        JoystickButton intakeUpButton =
-            new JoystickButton(driverController, XboxController.Button.kY.value);
+    // Run intake in reverse while held
+    JoystickButton reverseButton =
+        new JoystickButton(driverController.getHID(), XboxController.Button.kB.value);
 
-        intakeDownButton.toggleOnTrue(new IntakeDownCommand(intakeSubsystem));
-        intakeUpButton.toggleOnTrue(new IntakeUpCommand(intakeSubsystem));
-        intakeToggleButton.toggleOnTrue(new RunIntakeCommandForward(intakeSubsystem));
-        reverseButton.whileTrue(new RunIntakeCommandReversed(intakeSubsystem));
-    }
+    // Toggle intake to go down
+    JoystickButton intakeDownButton =
+        new JoystickButton(driverController.getHID(), XboxController.Button.kX.value);
+
+    // Toggle intake to go up
+    JoystickButton intakeUpButton =
+        new JoystickButton(driverController.getHID(), XboxController.Button.kY.value);
+
+    intakeDownButton.toggleOnTrue(new IntakeDownCommand(intakeSubsystem));
+    intakeUpButton.toggleOnTrue(new IntakeUpCommand(intakeSubsystem));
+    intakeToggleButton.toggleOnTrue(new RunIntakeCommandForward(intakeSubsystem));
+    reverseButton.whileTrue(new RunIntakeCommandReversed(intakeSubsystem));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -82,8 +129,7 @@ public class RobotContainer {
       case 0:
         return null; // do nothing
       case 1:
-        return new PrintCommand("1");
-        //DriveDistance(39, 0.3, m_robotDrive);
+        return null;
       case 2:
         return new PrintCommand("2");
         //DriveTimed(2.0, 0.3, m_robotDrive);
