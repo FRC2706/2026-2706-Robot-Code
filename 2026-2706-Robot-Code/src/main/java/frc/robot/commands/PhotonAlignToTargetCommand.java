f399551b49583204f0581565bee4a35d70318d2b
Ethan <ethan.soh360@gmail.com>
2026-03-14 14:12:42 -0400

    Changed Constants folder to UtilityConstants

--- 2026-2706-Robot-Code/src/main/java/frc/robot/commands/PhotonAlignToTargetCommand.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/commands/PhotonAlignToTargetCommand.java
@@ -7,7 +7,7 @@ package frc.robot.commands;
 // Imports
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.math.geometry.Translation2d;
-import frc.robot.Constants.VisionConstants;
+import frc.robot.UtilityConstants.VisionConstants;
 import frc.robot.subsystems.PhotonSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
