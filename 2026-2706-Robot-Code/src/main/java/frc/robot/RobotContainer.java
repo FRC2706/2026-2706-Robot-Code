27c55fbd5003b13f0a3f2c9e39cd388545fd0821
Patchy-pirate <jacobgloade@gmail.com>
2026-03-17 18:45:46 -0400

    removed double defined variables

--- 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -81,12 +81,11 @@ public class RobotContainer {
       // Toggle intake ON/OFF
     JoystickButton intakeToggleButton =
         new JoystickButton(m_operatorController.getHID(), XboxController.Button.kRightBumper.value);
-        new JoystickButton(m_operatorController.getHID(), XboxController.Button.kRightBumper.value);
 
     // Run intake in reverse while held
     JoystickButton reverseButton =
         new JoystickButton(m_operatorController.getHID(), XboxController.Button.kLeftBumper.value);
-        new JoystickButton(m_operatorController.getHID(), XboxController.Button.kLeftBumper.value);
+
 
     // Toggle intake to go down
     Trigger intakeDownButton =
