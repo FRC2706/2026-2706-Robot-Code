2df009d33b664fbcf986476eb9a2953cf1813c7f
GavinWu42711 <gavinwu42711@gmail.com>
2026-03-17 21:19:00 -0400

    Removing print
    Removing print

--- 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -128,7 +128,6 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
 
     //Get the auto based on autonomous selector switch
-    System.out.println(m_autoSelectorKnobSubsystem.getAutoMode());
     Command auto = m_autoPlans.getAutonomousCommand(m_autoSelectorKnobSubsystem.getAutoMode());
 
     return auto;
