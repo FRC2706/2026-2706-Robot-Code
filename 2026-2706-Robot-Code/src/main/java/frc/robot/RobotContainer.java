fe3b65458d19bb91cbde7b70cb57e9de4465aa87
GavinWu42711 <gavinwu42711@gmail.com>
2026-04-09 20:32:59 -0400

    Temporary FMS

--- 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2026-2706-Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import frc.robot.commands.StopShooter;
 
 import frc.robot.subsystems.AutoSelectorKnobSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.TempFMS;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -67,6 +68,8 @@ public class RobotContainer {
   private final CommandXboxController driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
   private final CommandXboxController m_operatorController = new CommandXboxController(OperatorConstants.kOperatorControllerPort);
   private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
+
+  private final TempFMS m_TempFMS = new TempFMS(m_PhotonSubsystem, m_ShooterSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
